6891a9aee9e60d53dcede7409c19e20c11019094
Aleksandr Meshkov <aleksandr.meshkov@ittc.ru>
2022-11-13 03:15:44 +0300

    add app2 for testcontainer

---
+++ app2/src/test/java/com/example/app2/App2ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.app2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class App2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
